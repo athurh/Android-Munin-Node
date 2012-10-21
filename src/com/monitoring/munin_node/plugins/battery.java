5f5727f3b5b5292b052d75d77441c0d35944f97b
athurh <athurh@gmail.com>
2012-10-21 03:16:53 +0200

    plugins: battery: fix temp and voltage values and set charging as AREA

--- src/com/monitoring/munin_node/plugins/battery.java
+++ src/com/monitoring/munin_node/plugins/battery.java
@@ -21,14 +21,18 @@ public class battery implements Plugin_API {
 	public String getCat() {
 		return "Android Phone";
 	}
+
+	@Override
 	public Boolean needsContext() {
 		return true;
 	}
+
+	@Override
 	public Void setContext(Context newcontext) {
 		context = new ContextWrapper(newcontext);
 		return null;
 	}
-	
+
 	@Override
 	public Void run(Handler handler) {
 		StringBuilder output = new StringBuilder();
@@ -38,52 +42,45 @@ public class battery implements Plugin_API {
 		output.append("graph_vlabel %\n");
 		output.append("graph_category Android Phone\n");
 		output.append("graph_info This graph shows battery charge in %\n");
-		output.append("battery.label Battery Charge\n");
 		output.append("charge.label Charging\n");
+		output.append("charge.draw AREA\n");
+		output.append("charge.colour E0E0E0\n");
+		output.append("battery.label Battery charge\n");
 		output.append("multigraph Battery_Temp\n");
 		output.append("graph_title Battery Temp\n");
 		output.append("graph_vlabel Temp\n");
 		output.append("graph_category Android Phone\n");
-		output.append("graph_info This graph shows battery Temp\n");
-		output.append("temp.label Battery Temperature\n");
+		output.append("graph_info This graph shows battery temp\n");
+		output.append("temp.label Battery temperature\n");
 		output.append("multigraph Battery_Volt\n");
 		output.append("graph_title Battery Voltage\n");
 		output.append("graph_vlabel Voltage\n");
 		output.append("graph_category Android Phone\n");
-		output.append("graph_info This graph shows battery Voltage\n");
-		output.append("volt.label Battery Voltage");
+		output.append("graph_info This graph shows battery voltage\n");
+		output.append("volt.label Battery voltage");
 		StringBuilder output2 = new StringBuilder();
 		Intent Battery = context.registerReceiver(null, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
 		Float Battery_unscaled = new Float(Battery.getIntExtra("level", 0));
 		Float Battery_scale = new Float(Battery.getIntExtra("scale", 0));
 		Float Battery_value = (Battery_unscaled/Battery_scale)*100;
-		output2.append("battery.value "+Battery_value.toString()+"\n");
-		//output.append(Battery_scale.toString()+"\n");
 		if(Battery.getIntExtra("plugged", 0)>0){
 			output2.append("charge.value 100\n");
-		}
-		else{
+		} else {
 			output2.append("charge.value 0\n");
 		}
+		output2.append("battery.value "+Battery_value.toString()+"\n");
 		output2.append("multigraph Battery_Temp\n");
-		Float temp = new Float(Battery.getIntExtra("voltage", 0)/100);
+		Float temp = new Float(Battery.getIntExtra("temperature", 0))/10;
 		output2.append("temp.value "+temp.toString()+"\n");
 		output2.append("multigraph Battery_Volt\n");
-		Float volt = new Float(Battery.getIntExtra("temperature",0)/100);
-		output2.append("volt.value "+volt);
+		Float volt = new Float(Battery.getIntExtra("voltage", 0))/1000;
+		output2.append("volt.value "+volt.toString());
 		Bundle bundle = new Bundle();
 		bundle.putString("name", this.getName());
 		bundle.putString("config", output.toString());
 		bundle.putString("update", output2.toString());
 		Message msg = Message.obtain(handler, 42, bundle);
 		handler.sendMessage(msg);
-		output = null;
-		output2 = null;
-		Battery_unscaled = null;
-		Battery_scale = null;
-		Battery_value = null;
-		temp = null;
-		volt = null;
 		return null;
 	}
 }
